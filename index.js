201d5b95ce3f09d4675f5787e86515c1cd3cfc25
pankaj <pankajkumar70792@gmail.com>
2024-01-04 19:41:21 +0530

    Create and fetch

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 const express = require("express");
-const { User } = require("./models");
+const { User, sequelize } = require("./models");
 
 const app = express();
 
 app.use(express.json());
 
-app.post("/createUser");
+app.post("/createUser", async function (req, res) {
+  const { name, email } = req.body;
+  try {
+    const users = await User.create({ name, email });
+    res.send(users);
+  } catch (err) {
+    console.log("err", err);
+    res.status(500).send({ err: "data creation failer" });
+  }
+});
+
+app.get("/getUsers", async function (req, res) {
+  try {
+    const user = await User.findAll();
+    res.send(user);
+  } catch (err) {
+    console.log("err", err);
+    res.status(500).send({ err: "fetch error" });
+  }
+});
 
 app.listen(3000, async () => {
   console.log("server is running");
-  await User.sync({ force: true });
+  await sequelize.authenticate();
 });
